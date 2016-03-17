dc51a059bf4aaf2b23fb09208ac27e4255b7d037
okubyou <okubyou.0398@gmail.com>
2016-03-17 19:01:42 +0900

    WIP

---
+++ Assets/res/scripts/AoiSoundManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AoiSoundManager : MonoBehaviour {
+
+    [SerializeField] AudioClip sound1;
+    AudioSource audioSource;
+
+    // Use this for initialization
+    void Start (){
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update (){
+	
+	}
+
+    void play(){
+
+    }
+
+}
