85b0e43d147f4c88961360763e17aad43e1b44dd
okubyou <okubyou.0398@gmail.com>
2016-05-08 11:28:16 +0900

    GW作業分

---
+++ Assets/res/scripts/Aim.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Aim : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(1)){
+            //GetComponent("").enabled = true;
+        }
+	}
+}
