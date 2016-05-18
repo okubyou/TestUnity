6df1ef393190647888282b38496bfea24f23e4ff
okubyou <okubyou.0398@gmail.com>
2016-05-19 01:30:15 +0900

    2016/05/19 作業分

--- Assets/res/scripts/Aim.cs
+++ Assets/res/scripts/Aim.cs
@@ -12,15 +12,26 @@ public class Aim : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetMouseButton(1)){
+
+        if(Input.GetMouseButton(0)) {
+            animator.SetBool("fire", true);
+        } else {
+            animator.SetBool("fire", false);
+        }
+
+        if(Input.GetMouseButton(1)) {
             animator.SetBool("aim", true);
-        
+            gameObject.GetComponent<HeadLookController>().enabled = true;
+
             if(Vector3.Angle(
-                new Vector3(transform.forward.x, 0, transform.forward.z), 
+                new Vector3(transform.forward.x, 0, transform.forward.z),
                 new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z)
-                ) > 45
-            ) 
-            transform.forward = new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z);
+                ) > 90
+            )
+                transform.forward = new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z);
+        } else{
+            animator.SetBool("aim", false);
+            gameObject.GetComponent<HeadLookController>().enabled = false;
         }
     }
 
